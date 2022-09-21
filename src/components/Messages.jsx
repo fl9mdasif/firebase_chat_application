62518654744e5492b8bca34243ee1d0342378ba0
fl9mdasif <freelancer.mdasifofficial@gmail.com>
2022-09-21 17:29:39 +0600

    eork done 99%

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,17 +1,36 @@
-import React from 'react'
+import { doc, onSnapshot } from 'firebase/firestore';
+import React, { useContext } from 'react'
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { ChatContext } from '../context/ChatContext';
+import { db } from '../firebase.init';
 import { Message } from './Message'
 
 export const Messages = () => {
+
+    const { data } = useContext(ChatContext);
+    const [messages, setMessages] = useState([]);
+
+    useEffect(() => {
+        const unsub = onSnapshot(doc(db, "chats", data.chatId), (doc => {
+            doc.exists() && setMessages(doc.data()?.messages)
+        }))
+
+        return () => {
+            unsub()
+        }
+    }, [data.chatId])
+    console.log(messages)
+
     return (
         <div className="messages">
-            <Message />
-            <Message />
-            <Message />
-            <Message />
-            <Message />
-            <Message />
-            <Message />
-            <Message />
+
+            {
+                messages.map((m) => (
+                    <Message message={m} key={m.id} />
+                ))
+            }
+
         </div>
     )
 }
